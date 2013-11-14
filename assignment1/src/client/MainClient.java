1d46f6249c12899ffd4dd3834b871c0fb327dbc5
Austin Dubina <austin.dubina@jivesoftware.com>
2013-11-14 11:44:36 -0800

    implemented Mainclient

---
+++ assignment1/src/client/MainClient.java
@@ -0,0 +1,12 @@
+package client;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: austin.dubina
+ * Date: 11/14/13
+ * Time: 11:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainClient {
+    public static void main(String srgs[] throws IOExcpetion)
+}
